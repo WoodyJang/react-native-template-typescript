4a3999707ef6b99153830138ded59bb798d15a82
woodyjang <zhangwu@dxy.cn>
2019-09-28 22:41:28 +0800

    test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # react-native-template-typescript
 react-native-template-typescript
+测试
--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,7 @@ import {Button, Provider, Toast} from '@ant-design/react-native'
 
 class App extends Component<{}, {}> {
   render() {
+    console.log(111)
     return (
       <Provider>
         <Button onPress={() => Toast.info('This is a toast tips')}> Start </Button>
